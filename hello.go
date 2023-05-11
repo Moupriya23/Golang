296f0359e5b0e56a71385b96917ad59b7711d0bd
Moupriya Roy <122144586+Moupriya23@users.noreply.github.com>
2023-05-11 16:19:08 +0530

    hello.go

--- hello.go
+++ hello.go
@@ -1,17 +1,10 @@
 package main
-
-// import multiple packages
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	// convert the string to lowercas
-	lower := strings.ToLower("GOLANG STRINGS")
-	fmt.Println(lower)
-	// convert the string to uppercase
-	upper := strings.ToUpper("golang strings")
-	fmt.Println(upper)
+	
+	fmt.Println("Hi Golang")
 
 }
